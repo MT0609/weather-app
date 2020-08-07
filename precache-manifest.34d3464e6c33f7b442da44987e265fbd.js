3f4745c5e77b609031b7dde05639e65b6bf90c5f
MT0609 <thang123lk@gmail.com>
2020-08-07 11:22:38 +0700

    Updates

---
+++ precache-manifest.34d3464e6c33f7b442da44987e265fbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db8c409a493e1b3aa21411cf029a963",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "81f910b942afe79e6551",
+    "url": "/weather-app/static/css/main.e06ca736.chunk.css"
+  },
+  {
+    "revision": "848f901467d002e1a710",
+    "url": "/weather-app/static/js/2.58cafa9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.58cafa9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81f910b942afe79e6551",
+    "url": "/weather-app/static/js/main.e77ef207.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
